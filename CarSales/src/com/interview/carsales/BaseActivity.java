9708ccbd1f28f3e443d5410607b09f971ba443ac
mahalelabs <mahale.labs@gmail.com>
2013-07-03 23:42:03 +0530

    Update BaseActivity.java

--- CarSales/src/com/interview/carsales/BaseActivity.java
+++ CarSales/src/com/interview/carsales/BaseActivity.java
@@ -12,7 +12,7 @@ public class BaseActivity extends Activity implements IServiceCallbacks{
 	@Override
 	public void OrderSatus(List<Order> orders) {
 		// TODO Auto-generated method stub
-		
+		//Testing............................
 	}
 
 }
